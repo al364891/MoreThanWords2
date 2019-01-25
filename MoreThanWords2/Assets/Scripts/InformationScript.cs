6b361cb402d3438b5970c32b13b53348f4703df0
Ruben <al341510@uji.es>
2019-01-25 17:32:39 +0100

    Bloqueo de flechas, story

--- MoreThanWords2/Assets/Scripts/InformationScript.cs
+++ MoreThanWords2/Assets/Scripts/InformationScript.cs
@@ -16,6 +16,8 @@ public class InformationScript : MonoBehaviour {
     public Text negativeScore;
     public Text timeScore;
 
+    public Text storyText;
+
     private void Awake()
     {
         manager = GameObject.FindGameObjectWithTag("Manager");
@@ -27,6 +29,7 @@ public class InformationScript : MonoBehaviour {
         killScore.text = "Killing Points: " + scoreManager.getKillScore();
         negativeScore.text = "Fault Points: " + scoreManager.getNegativeScore();
         timeScore.text = "Time Bonus: " + scoreManager.getTimeScore();
+        storyText.text = scoreManager.getStoryText();
         nextScene = scoreManager.getNextScene();
 
         Destroy(manager);
